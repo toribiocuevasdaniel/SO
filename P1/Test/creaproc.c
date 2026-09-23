ff996eb39bb46e42ec9425170bac08ad1c001559
Daniel Toribio <toribiocuevasdaniel@gmail.com>
2026-09-23 10:04:55 +0200

    Primeros códigos de prueba

--- P1/Test/creaproc.c
+++ P1/Test/creaproc.c
@@ -1,3 +1,4 @@
+// Primer código de ejemplo de la práctica
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,17 +8,17 @@ pid_t pid;
 pid = fork();
 switch (pid)
 {
-case –1:
+case -1:
 printf ("No he podido crear el proceso hijo \n");
 break;
 case 0:
-4
+
 printf ("Soy el hijo, mi PID es %d y mi PPID es %d \n",
 getpid(), getppid());
 sleep (20);
 break;
 default:
-printf ("Soy  el padre, mi PID es %d y el PID de mi hijo es %d \n",
+printf ("Soy el padre, mi PID es %d y el PID de mi hijo es %d \n",
 getpid(), pid);
 sleep (30);
 }
